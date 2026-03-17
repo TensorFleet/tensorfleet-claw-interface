108d2a50de105f4d628c3501ff45acf816a103d5
darthLeviN <darthleviellis@gmail.com>
2026-03-17 13:36:54 +0530

    make ros-connect executor wip

--- packages/tensorfleet-tools/src/index.ts
+++ packages/tensorfleet-tools/src/index.ts
@@ -2,5 +2,6 @@ export { executeEntityRead } from "./tools/entity-read-executor";
 export { executeRosNodeRead } from "./tools/ros-node-read-executor";
 export { executeRosTopicRead } from "./tools/ros-topic-read-executor";
 export { executeRosServiceRead } from "./tools/ros-service-read-executor";
+export { executeRosConnect } from "./tools/ros-connect-executor";
 export { logger } from "./logger";
 export type { ToolAPI, ToolDefinition, ToolParameters, ToolExecutionResult } from "./tool-api";
