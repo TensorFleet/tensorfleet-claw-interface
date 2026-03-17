0335470f41ad2c49f5f2d6930bb52619a2d22293
darthLeviN <darthleviellis@gmail.com>
2026-03-17 12:14:02 +0530

    refactor schema loader

--- packages/tensorfleet-tools/src/tools/ros-topic-read.ts
+++ packages/tensorfleet-tools/src/tools/ros-topic-read.ts
@@ -1,18 +1,10 @@
 import { Type } from "@sinclair/typebox";
-import * as fs from "fs";
-import * as path from "path";
 import { TensorfleetTelemetryRosTopicRead } from "../schema-types/tensorfleet-telemetry.ros-topic.read.input";
 import { loadTensorfleetConfig } from "../config-loader";
 import { setupWindowMock, validateProxyConfig } from "../window-mock";
 import { logger } from "../logger";
 import { ToolAPI } from "../tool-api";
-
-// Helper function to load schema from file
-function loadSchema(filename: string) {
-  const schemaPath = path.join(__dirname, "../schema", filename);
-  const schemaContent = fs.readFileSync(schemaPath, "utf8");
-  return JSON.parse(schemaContent);
-}
+import { loadSchema } from "../schema-loader";
 
 export function registerRosTopicReadTool(api: ToolAPI) {
   api.registerTool({
